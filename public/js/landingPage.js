222306edb4a818949d53b06a8f6964d73cdddfae
sammidunn <sammidunn@hotmail.com>
2021-08-02 17:20:13 +0000

    create API request URL with user inputted symbol

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -1,24 +1,24 @@
 const queryField = document.querySelector("#search");
+const submitButton = document.querySelector("#submit");
 
-// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo';
+submitButton.addEventListener("click", (e) => {
+    
+    let myKey = 'UBT7AODFWQYQD0KN';
+    console.log("raw query: ", queryField.value);
+    let sym = encodeURIComponent(queryField.value.toUpperCase());
+    console.log("encoded: ", sym);
+    const urlToFetch = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${sym}&interval=5min&apikey=${myKey}`;
 
-let myKey = '';
-  //console.log("raw query: ", queryField.value);
-  let sym = encodeURIComponent(queryField.value);
-  //console.log("encoded: ", topic);
-  const urlToFetch = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${myKey}`;
-  
-  //console.log(topic);
-  //console.log(urlToFetch);
-  
-  fetch(urlToFetch)
-    .then(response => response.json())
-    .then(myJson => {
-    //console.log(myJson.data[0].images.original.url);
-    const imgUrl = myJson.data[getRandom(25)].images.original.url;
-    imageHolderDiv.innerHTML = `<img src="${imgUrl}"  />`;
-  })
-  .catch(error => {
-    console.log("error: ", error)
-  })
+    console.log(urlToFetch);
+
+    fetch(urlToFetch)
+        .then(response => response.json())
+        .then(myJson => {
+            //console.log(myJson.data[0].images.original.url);
+            //const imgUrl = myJson.data[getRandom(25)].images.original.url;
+            //imageHolderDiv.innerHTML = `<img src="${imgUrl}"  />`;
+        })
+        .catch(error => {
+            console.log("error: ", error)
+        })
+})
