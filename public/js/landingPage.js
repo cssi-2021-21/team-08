fdc8376b3cbdb7878082dacbe4019d7f622c2992
sammidunn <sammidunn@hotmail.com>
2021-08-02 17:00:37 +0000

    beginning to pull from api

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -1,22 +1,24 @@
-console.log("script running");
-
-'use strict';
-var request = require('request');
+const queryField = document.querySelector("#search");
 
 // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=UBT7AODFWQYQD0KN';
+var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo';
 
-request.get({
-    url: url,
-    json: true,
-    headers: {'User-Agent': 'request'}
-  }, (err, res, data) => {
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-      // data is successfully parsed as a JSON object:
-      console.log(data);
-    }
-});
+let myKey = '';
+  //console.log("raw query: ", queryField.value);
+  let sym = encodeURIComponent(queryField.value);
+  //console.log("encoded: ", topic);
+  const urlToFetch = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${myKey}`;
+  
+  //console.log(topic);
+  //console.log(urlToFetch);
+  
+  fetch(urlToFetch)
+    .then(response => response.json())
+    .then(myJson => {
+    //console.log(myJson.data[0].images.original.url);
+    const imgUrl = myJson.data[getRandom(25)].images.original.url;
+    imageHolderDiv.innerHTML = `<img src="${imgUrl}"  />`;
+  })
+  .catch(error => {
+    console.log("error: ", error)
+  })
