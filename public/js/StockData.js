3fba19b5449bfbe89c309a936c96c08209625362
sammidunn <sammidunn@hotmail.com>
2021-08-03 16:55:58 +0000

    resolved error in data filtering

--- public/js/StockData.js
+++ public/js/StockData.js
@@ -6,37 +6,11 @@ class StockData {
         this.changePct = null;
         this.change = null;
         this.logo = null;
+        
+        this.news = [];
 
-        //const urlToFetch = `https://ticker-2e1ica8b9.now.sh//keyword/${sym}`;
-        //const urlToFetch = `https://cloud.iexapis.com/stable/${sym}/chart/1m?token=pk_${apiKey}&includeToday=true`; //production
-        //const urlToFetch = `https://sandbox.iexapis.com/stable/stock/${sym}/chart/1m?token=${apiKey}&includeToday=true`; //sandbox --> chart endpoint = historical data 
-        const urlToFetch = `https://sandbox.iexapis.com/stable/stock/${this.symbol}/quote/?token=${apiKey}`; //sandbox --> quote endpoint = real-time data
-        console.log(urlToFetch);
-        fetch(urlToFetch)
-            .then(response => response.json())
-            .then(myJson => {
-                this.symbol = myJson["symbol"];
-                this.company = myJson["companyName"];
-                this.latestPrice = myJson["latestPrice"];
-                this.changePct = myJson["changePercent"];
-                this.change = myJson["change"];
-                })
-            .catch(error => {
-                console.log("error: ", error)
-            });
-
-        /*const urlToFetchLogo = `https://sandbox.iexapis.com/stable/stock/${this.symbol}/logo/?token=${apiKey}`; //sandbox --> quote endpoint = real-time data
-        console.log(urlToFetchLogo);
-        fetch(urlToFetchLogo)
-            .then(response => response.json())
-            .then(myJson => {
-                this.logo = myJson["url"];
-                })
-            .catch(error => {
-                console.log("error: ", error)
-            });*/
-        let logo = this.getRequest("logo");
-        console.log("logo:", logo);
+        this.getRequest("quote");
+        this.getRequest("logo");
     }
 
     getRequest(request) {
@@ -50,36 +24,41 @@ class StockData {
         fetch(urlToFetch)
             .then((response) => response.json())
             .then((myJson) => {
-                console.log(myJson);
-                return myJson;
+                this.filterData(request, myJson);
             })
             .catch((error) => {
                 console.log("error: ", error)
             });
-        
-        console.log("exiting getRequest()")
+    }
+
+    filterData(type, data) {
+        console.log("entering filter");
+        if(type === "logo") {
+            this.logo = data["url"];
+            console.log(this);
+        }
+        else if (type === "quote"){
+            this.company = data["companyName"];
+            this.latestPrice = data["latestPrice"];
+            this.changePct = data["changePercent"];
+            this.change = data["change"];
+        }
+        else if(type === "news/last/10"){
+            for(let i = 0; i < data.length; i++) {
+                this.news.push({
+                    headline: data[i]["headline"],
+                    url: data[i]["url"],
+                    img: data[i]["image"]
+                });
+            }
+        }
     }
 
     headlines() {
-        let articles = [];
-        const urlToFetch = `https://sandbox.iexapis.com/stable/stock/${this.symbol}/news/last/10/?token=${apiKey}`; //sandbox --> quote endpoint = real-time data
-        console.log(urlToFetch);
-        fetch(urlToFetch)
-            .then(response => response.json())
-            .then(myJson => {
-                for(let i = 0; i < myJson.length; i++) {
-                    articles.push({
-                        headline: myJson[i]["headline"],
-                        url: myJson[i]["url"],
-                        img: myJson[i]["image"]
-                    });
-                }
-            })
-            .catch(error => {
-                console.log("error: ", error)
-            });
-        return articles;
+        this.getRequest("news/last/10")        
+        return this.news;
     }
+
 }
 export{StockData};
 
