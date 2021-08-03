bba5f82543b3dd9ff9896cf8164747332862e2ac
sammidunn <sammidunn@hotmail.com>
2021-08-03 18:08:13 +0000

    attempting to populate cards with stock data

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -1,7 +1,22 @@
 import {StockData} from '/js/StockData.js';
+//import {cardsData} from '/js/cardsData.js';
 
 const queryField = document.querySelector("#search");
 const submitButton = document.querySelector("#submit");
+
+/*
+const baseStocks = ["googl", "aapl", "amzn", "baba", "msft", "tsla"];
+for(let i = 0; i < baseStocks.length; i++) {
+    let base = baseStocks[i];
+    let stock = new StockData(base);
+
+    cardsData[i]["avatar"] = stock.logo;
+    cardsData[i]["user"] = {
+        name: stock.company,
+        handle: stock.sym,
+    }
+
+}*/
 
 submitButton.addEventListener("click", (e) => {
     console.log("raw query: ", queryField.value);
@@ -11,8 +26,15 @@ submitButton.addEventListener("click", (e) => {
     let stock = new StockData(sym);
 });
 
-const displayStockCard = (stock) => {
-    return `
-        //updating html of cards using general stock data
-    `;
-}
+
+/*
+id: 1,
+  image: 'http://sevensreport.com/wp-content/uploads/2016/07/stock-market-3.jpg',
+  avatar: 'https://avatars..com/api/initials/john%20doe.svg',
+  user: {
+    name: 'StockName',
+    handle: 'ID',
+    title: 'Company Name'
+  },
+  content: 'Description of Company'
+  */
