0b961409b37b56b3492bb6fe073b829e584b0c54
sammidunn <sammidunn@hotmail.com>
2021-08-04 23:25:33 +0000

    calls to/from database working

--- public/js/StockData.js
+++ public/js/StockData.js
@@ -7,11 +7,9 @@ class StockData {
         this.changePct = 0;
         this.change = 0;
         this.logo = null;
-        this.news = [];
 
         this.getInfo();
         this.getLogo();
-       // this.getNews();
     }
 
     getRequest(request)
@@ -49,92 +47,18 @@ class StockData {
         }
     }
 
-    getNews() {
-        let data = this.getRequest("news/last/10");
+    getNews(entries) {
+        let data = this.getRequest(`news/last/${entries}`);
+        let news = [];
         for(let i = 0; i < data.length; i++) {
-            this.news.push({
+            news.push({
                 headline: data[i]["headline"],
                 url: data[i]["url"],
                 img: data[i]["image"]
             });
         }
+        return news;
     }
-
-   /* getRequest(request) {
-        //const urlToFetch = `https://ticker-2e1ica8b9.now.sh//keyword/${sym}`;
-        //const urlToFetch = `https://cloud.iexapis.com/stable/${sym}/chart/1m?token=pk_${apiKey}&includeToday=true`; //production
-        //const urlToFetch = `https://sandbox.iexapis.com/stable/stock/${sym}/chart/1m?token=${apiKey}&includeToday=true`; //sandbox --> chart endpoint = historical data 
-        const urlToFetch = `https://sandbox.iexapis.com/stable/stock/${this.symbol}/${request}/?token=${apiKey}`; //sandbox --> quote endpoint = real-time data
-       
-        fetch(urlToFetch)
-            .then((response) => response.json())
-            .then((myJson) => {
-                this.filterData(request, myJson);
-            })
-            .catch((error) => {
-                console.log("error: ", error)
-            });
-    }
-
-    getRequest3 = async (request) => {
-        const myJson = await this.getRequest2(request);
-        console.log("req3:", myJson);
-        //return myJson;
-    }
-
-    async getRequest2(request) {
-        //const urlToFetch = `https://ticker-2e1ica8b9.now.sh//keyword/${sym}`;
-        //const urlToFetch = `https://cloud.iexapis.com/stable/${sym}/chart/1m?token=pk_${apiKey}&includeToday=true`; //production
-        //const urlToFetch = `https://sandbox.iexapis.com/stable/stock/${sym}/chart/1m?token=${apiKey}&includeToday=true`; //sandbox --> chart endpoint = historical data 
-        const urlToFetch = `https://sandbox.iexapis.com/stable/stock/${this.symbol}/${request}/?token=${apiKey}`; //sandbox --> quote endpoint = real-time data
-       
-        let resp = fetch(urlToFetch)
-        .then((response) => response.json())
-        .then((myJson) => {
-            return myJson;
-        })
-        .catch((error) => {
-            console.log("error: ", error)
-        });
-
-
-        if (! resp.ok) {
-            console.log('error: ', resp.status)
-
-            return;
-        }
-
-        let myJson = await resp.json();
-
-        return myJson;
-
-            
-    }
-
-    filterData(type, data) {
-        if(type === "logo") {
-            this.logo = data["url"];
-        }
-        else if (type === "quote"){
-            this.company = data["companyName"];
-            console.log(this.company);
-
-            this.latestPrice = data["latestPrice"];
-            this.changePct = data["changePercent"];
-            this.change = data["change"];
-
-
-        }
-        else if(type === "news/last/10"){
-            for(let i = 0; i < data.length; i++) {
-                this.news.push({
-                    headline: data[i]["headline"],
-                    url: data[i]["url"],
-                    img: data[i]["image"]
-                });
-            }
-        }
-    }*/
 }
 
 export{StockData};
--- public/js/stockPortfolio.js
+++ public/js/stockPortfolio.js
@@ -1,5 +1,26 @@
 import {StockData} from '/js/StockData.js';
-let googleUserId;
+let userId = "testID";
+
+
+
+/*
+window.onload = (event) => {
+  // Use this to retain user state between html pages.
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log('Logged in as: ' + user.displayName);
+      userId = user.uid;
+      console.log(getPositions("testID"));
+
+    } else {
+      // If not logged in, navigate back to login page.
+      //window.location = 'index.html';
+      console.log("not logged in");
+    };
+  });
+};*/
+
+
 
 document.querySelector("#testBuy").addEventListener("click", (e) => {
     saveBuy();
@@ -27,12 +48,12 @@ const getQuote = (sym) => {
 }
 
 const addToPortfolio = (sym) => {
-    setTransaction("testID", "watch", sym, 0);
+    setTransaction(userId, "watch", sym, 0);
 }
 
 const saveBuy = () => {
-    setTransaction("testID", "buy", "googl", 100);
-    setTransaction("testID", "sell", "googl", 50);
+    setTransaction(userId, "buy", "googl", 100);
+    setTransaction(userId, "sell", "googl", 50);
 }
 
 const setTransaction = (user, type, sym, volume) => {
@@ -53,3 +74,71 @@ const setTransaction = (user, type, sym, volume) => {
         })
     }
 }
+
+const getPositions = () => {
+    const ref = firebase.database().ref(`users/${userId}/portfolio`);
+    let positions = [];
+
+    ref.on('value', (snapshot) => {
+        const syms = snapshot.val();
+        
+        console.log("start iterating...");
+
+        for (let stock in syms) {
+
+            const  transactions = syms[stock];
+            
+            let buys = 0;
+            let sumBuyPrice = 0;
+            let sumBuyShares = 0;
+            let sumBuyCost = 0;
+
+            let sells = 0;
+            let sumSellPrice = 0;
+            let sumSellShares = 0;
+            let sumSellCost = 0;
+            for(const transaction in transactions) {
+                let trans = transactions[transaction];
+
+                if(trans.type === "buy") {
+                    buys++;
+                    sumBuyPrice += trans.cost;
+                    sumBuyShares += trans.shares;
+                    sumBuyCost += (trans.cost * trans.shares);
+                }
+                else if (trans.type === "sell") {
+                    sells++;
+                    sumSellPrice += trans.cost;
+                    sumSellShares += trans.shares;
+                    sumSellCost += (trans.cost * trans.shares);
+                }
+            };
+
+            sumBuyPrice = (buys > 0) ? sumBuyPrice = sumBuyPrice / buys : 0.0;
+            sumSellPrice = (sells > 0) ? sumSellPrice = sumSellPrice / sells : 0.0;
+
+            positions.push({
+                "sym": stock,
+                "avgBuy": sumBuyPrice,
+                "avgSell": sumSellPrice,
+                "remShares": sumBuyShares - sumSellShares,
+                "totalCost": sumBuyCost
+            });
+        };
+
+        console.log("end iterating.");
+        console.log(positions);
+        
+        renderPositionsHTML(positions);
+    });    
+}
+
+const renderPositionsHTML = (data) => {
+    //update the card data to display the positions data for each stock in the portfolio
+    console.log(data);
+
+    let tsla = new StockData("tsla");
+    console.log(tsla.getNews(10));
+}
+
+getPositions(userId);
